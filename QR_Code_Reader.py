2da23274f5b60513a69d4783f56e941e4707b886
DeniseIra02 <irajubilo@gmail.com>
2022-03-02 13:05:24 +0800

    add for loop statement for web cam scanning. Successfully create the program for qr code reading

--- QR_Code_Reader.py
+++ QR_Code_Reader.py
@@ -1,7 +1,7 @@
 import cv2
 import pyzbar.pyzbar as pyzbar
 import datetime
-
+import numpy as np
 
 #def function for output
 def barc_read(frame):
@@ -10,6 +10,36 @@ def barc_read(frame):
     #set module pyzbar and funtion decode
     d_barcodes = pyzbar.decode(frame)
 
+    #for loop for webcam scanning
+    for barc in d_barcodes:
+        #dcode the data
+        qR_code = barc.data.decode('utf-8')
+        print(qR_code + date_time.strftime("%c"))
+        #Add bounding box
+        d_pts = np.array([barc.polygon], np.int32)
+        d_pts = d_pts.reshape((-1,1,2))
+        #polyline function to create the bounding box
+        cv2.polylines(frame, [d_pts], True, (255,0,255), 5)
+        d_pts2 = barc.rect
+        #Add text for the title of the scanner box
+        cv2.putText(frame, "CONTACT TRACING", (d_pts2[0], d_pts2[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,0,255), 2)
+
+        #for saving the scanned data from qrcode in a text file
+        #set mode to 'a' for multiple data scanned
+        with open("Result.txt", mode = 'a') as datatxt:
+            datatxt.write("Scanned QR Code:\n")
+            datatxt.write(qR_code)
+            datatxt.write("\n")
+            datatxt.write("\n")
+            datatxt.write("\n")
+            datatxt.write(date_time.strftime("%c"))
+            datatxt.write("\n")
+            datatxt.write("\n")
+            datatxt.write("\n")
+            datatxt.close
+            
+    return barc_read
+         
 def forcam():
 
     web_camera = cv2.VideoCapture(0)
